a44e76df0600533b58e10c54cf69972be63f85b8
chicharron1 <soffiaromero900@gmail.com>
2025-10-06 16:08:03 -0600

    Añadí un formulario en la vista de inicio de sesión y un mensajito de error cuando los datos en el de registrarse no son válidos

--- bookity/BookityApp/urls.py
+++ bookity/BookityApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import inicio, login, registro
 
 urlpatterns = [
+    path('', inicio, name='inicio'),
     path('inicio/', inicio, name='inicio'),
     path('login/', login, name='login'),
     path('registro/', registro, name='registro'),
--- bookity/BookityApp/views.py
+++ bookity/BookityApp/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 def registro(request):
+    mensaje_error = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # Aquí puedes agregar lógica adicional, como iniciar sesión al usuario
-            return render(request, 'BookityApp/registro.html', {'form': UserCreationForm()})
+            return render(request, 'BookityApp/registro.html', {'form': UserCreationForm(), 'mensaje_error': ''})
+        else:
+            mensaje_error = "Inválido :("
     else:
         form = UserCreationForm()
-    return render(request, 'BookityApp/registro.html', {'form': form})
+    return render(request, 'BookityApp/registro.html', {'form': form, 'mensaje_error': mensaje_error})
 
 def inicio(request):
     return render(request, 'BookityApp/inicio.html')
 
 def login(request):
-    return render(request, 'BookityApp/login.html')
+    return render(request, 'BookityApp/login.html', {'form': AuthenticationForm()})

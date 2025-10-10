53ea4dfb650e89347b5094ea17238e2fdea22343
chicharron1 <soffiaromero900@gmail.com>
2025-10-10 11:13:05 -0600

    soy soffia y voy a borrar este trabajo

--- bookity/BookityApp/models.py
+++ bookity/BookityApp/models.py
@@ -5,7 +5,7 @@ class Publicacion(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
     contacto = models.CharField(max_length=100)
-    tipo = models.CharField(max_length=20, choices=[('Venta', 'Venta'), ('Intercambio', 'Intercambio'), ('Donación', 'Donación')], default='Donación')
+    tipo = models.CharField(max_length=20, choices=[('Intercambio', 'Intercambio'), ('Donación', 'Donación')], default='Donación')
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     ubicacion = models.CharField(max_length=100)
